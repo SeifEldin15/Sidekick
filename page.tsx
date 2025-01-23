025a2f439e4dee9430a080410ce09006979cfd86
seifeldin15 <seifeldin02@outlook.com>
2025-01-24 00:03:08 +0200

    removed redundancies and added correct imports

--- page.tsx
+++ page.tsx
@@ -1,14 +1,9 @@
-import Image from "next/image"
-import Link from "next/link"
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-import { ArrowUpRight, ArrowRight } from "lucide-react"
-import Nav from "@/components/Nav/Nav"
+import Nav from "@/components/Nav"
 import Hero from "@/components/Hero/Hero"
 import Analytics from "@/components/HomePage/Analytics"
 import Services from "@/components/HomePage/Services"
 import Newsletter from "@/components/HomePage/Newsletter"
-import Contact from "@/components/HomePage/Contact"
+import Contact from "@/components/Contact"
 export default function Home() {
   return (
     <div className="min-h-screen">
