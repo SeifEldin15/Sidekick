b9ae7cb1c46c32f881981b4d28603a5d574cd480
seifeldin15 <seifeldin02@outlook.com>
2025-01-31 12:23:40 +0200

    Auth route for login

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,52 @@
+import NextAuth from 'next-auth'
+import { MongoDBAdapter } from '@auth/mongodb-adapter'
+import clientPromise from '@/lib/mongodb'
+import CredentialsProvider from 'next-auth/providers/credentials'
+import bcrypt from 'bcryptjs'
+
+const handler = NextAuth({
+  adapter: MongoDBAdapter(clientPromise),
+  providers: [
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" }
+      },
+      async authorize(credentials) {
+        const client = await clientPromise
+        const users = client.db().collection('users')
+        
+        const user = await users.findOne({ email: credentials?.email })
+        if (!user) throw new Error('No user found')
+        
+        const passwordMatch = await bcrypt.compare(credentials?.password || '', user.password)
+        if (!passwordMatch) throw new Error('Invalid password')
+        
+        return { id: user._id.toString(), email: user.email }
+      }
+    })
+  ],
+  session: {
+    strategy: 'jwt'
+  },
+  callbacks: {
+    async session({ session, token }) {
+      if (session?.user) {
+        session.user.id = token.sub
+      }
+      return session
+    },
+    async jwt({ token, user }) {
+      if (user) {
+        token.sub = user.id
+      }
+      return token
+    }
+  },
+  pages: {
+    signIn: '/login',
+  }
+})
+
+export { handler as GET, handler as POST }
