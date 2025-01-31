aa1c1979c4b9e856f832b550295e3482e4a21cda
seifeldin15 <seifeldin02@outlook.com>
2025-01-31 12:23:57 +0200

    Auth lib

---
+++ app/lib/auth.js
@@ -0,0 +1,38 @@
+import NextAuth from 'next-auth'
+import CredentialsProvider from 'next-auth/providers/credentials'
+import { MongoDBAdapter } from '@auth/mongodb-adapter'
+import clientPromise from '@/lib/mongodb'
+import bcrypt from 'bcryptjs'
+
+const handler = NextAuth({
+  adapter: MongoDBAdapter(clientPromise),
+  providers: [
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" }
+      },
+      async authorize(credentials) {
+        const client = await clientPromise
+        const users = client.db().collection('users')
+        
+        const user = await users.findOne({ email: credentials?.email })
+        if (!user) throw new Error('No user found')
+        
+        const passwordMatch = await bcrypt.compare(credentials?.password || '', user.password)
+        if (!passwordMatch) throw new Error('Invalid password')
+        
+        return { id: user._id.toString(), email: user.email }
+      }
+    })
+  ],
+  session: {
+    strategy: 'jwt'
+  },
+  pages: {
+    signIn: '/login',
+  }
+})
+
+export { handler as GET, handler as POST }
