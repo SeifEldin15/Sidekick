fbbf931fc7e94ee6d19fd7b767eaa759e6c0bb68
seifeldin15 <seifeldin02@outlook.com>
2025-01-31 12:30:44 +0200

    Session provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from 'next'
 import './globals.css'
+import { SessionProvider } from 'next-auth/react'
+import Providers from './SessionProvider'
 
 export const metadata: Metadata = {
   title: 'v0 App',
@@ -13,7 +15,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   )
 }
